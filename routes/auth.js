c891db6202b45534a4180878247b4c2a5b34894c
victorygainer@gmail.com <victorygainer@gmail.com>
2024-09-10 23:00:28 +0900

    Add .env to .gitignore and remove from tracking

--- routes/auth.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-const authController = require('../controllers/authController');
-
-router.get('/', authController.login);
-
-router.post('/admin_login_process', authController.login_process);
-
-module.exports = router;

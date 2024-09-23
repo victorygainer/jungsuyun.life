e9132fd09444dedd265cd7b20f1e70eec26cac16
victorygainer@gmail.com <victorygainer@gmail.com>
2024-09-23 23:12:07 +0900

    Redirect HTTP to HTTPS when not on localhost

--- public/js/adminScript.js
+++ public/js/adminScript.js
@@ -3,7 +3,7 @@
 /**********************************************************************/
 $(document).ready(function() {
 
-    //localhost가 아닌 경우 http로 접속 시 http로 리다이렉트  
+    //localhost가 아닌 경우 http로 접속 시 https로 리다이렉트  
     if(location.hostname != "localhost"){
     if(window.location.protocol == "http:"){
         window.location.protocol = "https:";
--- public/js/indexScript.js
+++ public/js/indexScript.js
@@ -2,7 +2,7 @@
 /* 화면초기화 및 초기설정                                                   */
 /**********************************************************************/
 $(document).ready(function() {
-  //localhost가 아닌 경우 http로 접속 시 http로 리다이렉트  
+  //localhost가 아닌 경우 http로 접속 시 https로 리다이렉트  
   if(location.hostname != "localhost"){
     if(window.location.protocol == "http:"){
       window.location.protocol = "https:";
